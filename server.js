b31feab1769a36ecda09b8062f5b9020c678b680
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-06-06 00:02:49 +0530

    server added

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const jsonServer = require("json-server");
-const fs = require("fs");
-const dotenv = require("dotenv");
-
-dotenv.config(); // Load environment variables from .env file
-
-// Ensure db.json exists
-if (!fs.existsSync("db.json")) {
-  console.error("Error: db.json file not found");
-  process.exit(1);
-}
-
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+require("dotenv").config();
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 server.use(router);
-
+// Listen to port
 const port = process.env.PORT || 3000;
-server.listen(port, (err) => {
-  if (err) {
-    console.error("Error starting server:", err);
-    process.exit(1);
-  }
-  console.log(`JSON Server is running on port ${port}`);
+server.listen(port, () => {
+  console.log(`JSON Server is running ${port}`);
 });
+
+// Export the Server API
+module.exports = server;
